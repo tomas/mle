6afbe5096d351b7c3a7a9f9ae87c015393e20cb2
Tomás Pollak <tomaspollak@gmail.com>
2016-12-07 17:58:25 -0800

    Simple example of a comment lines toggle plugin

---
+++ plugins/comment.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+M.name = "Comment Lines"
+M.pver = "1.0"
+
+local function toggle_comment_on(line_number)
+  line = get_buffer_at_line(line_number)
+  regex   = "// ?%s"
+  comment = "// "
+  column  = string.find(line, regex)
+
+  if column then
+    count = string.len(comment)
+    delete_chars_at_line(line_number, column-1, count)
+  else
+    first = string.find(line, "[^ \t]")
+    if not first or first < 2 then
+      prepend_buffer_at_line(line_number, comment)
+    else
+      insert_buffer_at_line(line_number, comment, first - 1)
+    end
+  end
+end
+
+function M.onload()
+  add_keybinding("C-/", "toggle_comment")
+end
+
+function M.before_cmd_grep()
+  if has_selection() then
+    selection = get_selection() -- start line, start col, end line, end col
+    local first_line = selection[0]
+    local last_line = selection[3] == 0 and selection[2]-1 or selection[2]
+    for i = first_line, last_line, 1 do
+      toggle_comment_on(i)
+    end
+  else
+    toggle_comment_on(current_line_number())
+  end
+
+  return res
+end
+
+return M
